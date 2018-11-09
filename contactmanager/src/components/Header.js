2577411fbd8d3c6389d1df19bfda680c700aab95
Nalawade <NALAWJX@goodmanmfg.com>
2018-11-09 16:40:38 -0600

    trivial changes

--- contactmanager/src/components/Header.js
+++ contactmanager/src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = (props) => {
-  const {branding} = props;
+const Header = props => {
+  const { branding } = props;
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
@@ -12,23 +12,23 @@ const Header = (props) => {
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">Home
+              <a href="/" className="nav-link">
+                Home
               </a>
             </li>
           </ul>
         </div>
       </div>
-      
     </nav>
   );
 };
 
 Header.defaultProps = {
-  branding : 'My App'
-}
+  branding: 'My App'
+};
 
 Header.propTypes = {
-  branding : PropTypes.string.isRequired
-}
+  branding: PropTypes.string.isRequired
+};
 
 export default Header;
