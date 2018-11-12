83fc797f8061d7d34bee994d9e91f6c4214e9352
Nalawade <jeetesh.nalawade@capgemini.com>
2018-11-11 20:12:06 -0600

    folder re-structuring form skeleton, onSubmit remaining

---
+++ contactmanager/src/components/contacts/AddContact.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+ class AddContact extends Component {
+  
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+  }
+  onChange = (e) => this.setState({[e.target.name] : e.target.value});
+
+  render() {
+    const {name, email, phone}= this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="name"></label>
+                <input  type="text" 
+                        className="form-control form-control-lg"
+                        placeholder="Enter name..."
+                        name="name"
+                        value={name}
+                        onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email"></label>
+                <input  type="email" 
+                        className="form-control form-control-lg"
+                        placeholder="Enter email..."
+                        name="email"
+                        value={email}
+                        onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone"></label>
+                <input  type="text" 
+                        className="form-control form-control-lg"
+                        placeholder="Enter phone..."
+                        name="phone"
+                        value={phone}
+                        onChange={this.onChange}
+                />
+              </div>
+              <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddContact
