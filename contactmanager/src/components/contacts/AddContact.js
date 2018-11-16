0c66e63dc0fc972961e5fb20510a24bbe9fac5a0
Nalawade <jeetesh.nalawade@capgemini.com>
2018-11-16 17:15:26 -0600

    periodic commit

--- contactmanager/src/components/contacts/AddContact.js
+++ contactmanager/src/components/contacts/AddContact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import uuid from 'uuid'
+import {Consumer} from '../../context.js';
 
  class AddContact extends Component {
   
@@ -10,52 +12,68 @@ import React, { Component } from 'react'
 
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+
+    const {name, email, phone} = this.state;
+
+    const newContact = {
+      id: uuid(),
+      name,
+      email,
+      phone
+    }
+    console.log(newContact)
   }
   onChange = (e) => this.setState({[e.target.name] : e.target.value});
 
   render() {
     const {name, email, phone}= this.state;
-    return (
-      <div className="card mb-3">
-        <div className="card-header">Add Contact
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label htmlFor="name"></label>
-                <input  type="text" 
-                        className="form-control form-control-lg"
-                        placeholder="Enter name..."
-                        name="name"
-                        value={name}
-                        onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email"></label>
-                <input  type="email" 
-                        className="form-control form-control-lg"
-                        placeholder="Enter email..."
-                        name="email"
-                        value={email}
-                        onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="phone"></label>
-                <input  type="text" 
-                        className="form-control form-control-lg"
-                        placeholder="Enter phone..."
-                        name="phone"
-                        value={phone}
-                        onChange={this.onChange}
-                />
-              </div>
-              <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
-            </form>
+    return(
+    <Consumer>
+      {value=>{
+        const {dispatch} = value;
+        return (
+          <div className="card mb-3">
+          <div className="card-header">Add Contact
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="name"></label>
+                  <input  type="text" 
+                          className="form-control form-control-lg"
+                          placeholder="Enter name..."
+                          name="name"
+                          value={name}
+                          onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email"></label>
+                  <input  type="email" 
+                          className="form-control form-control-lg"
+                          placeholder="Enter email..."
+                          name="email"
+                          value={email}
+                          onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone"></label>
+                  <input  type="text" 
+                          className="form-control form-control-lg"
+                          placeholder="Enter phone..."
+                          name="phone"
+                          value={phone}
+                          onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
+              </form>
+            </div>
           </div>
-        </div>
-      </div>
+        </div>  
+        )
+      }}
+    </Consumer>
     )
   }
 }
--- contactmanager/src/context.js
+++ contactmanager/src/context.js
@@ -11,7 +11,12 @@ const reducer = (state, action) => {
                 contacts : state.contacts.filter(
                     contact => contact.id !== action.payload)
                 };
-        default:
+        case 'ADD_CONTACT':
+            return {
+                ...state,
+                contacts : [action.payload, ...state.contacts]
+                };
+            default:
             return state;
     }
 }
